a01f29f351ae179f2c5c0ced39dba403194af63a
heroyik <72379775+heroyik@users.noreply.github.com>
2026-01-16 23:49:13 +0900

    fix: update Firebase API key in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.COL_ENG_CONFIG = {
     projectId: "engdb-11b7f",
     appId: "1:221994477836:web:2344c5b5230a266dd4c129",
     storageBucket: "engdb-11b7f.firebasestorage.app",
-    apiKey: "", // REPLACED: Key decommissioned as it was leaked. Rotate in Google Cloud Console.
+    apiKey: "AIzaSyBQLmWfQ1pe5pNnshAgMSyztNK0RMnK7bY", // REPLACED: Key decommissioned as it was leaked. Rotate in Google Cloud Console.
     authDomain: "engdb-11b7f.firebaseapp.com",
     messagingSenderId: "221994477836",
     measurementId: "G-QCHLDW446Y",
