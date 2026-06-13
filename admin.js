75869876bc68ba336f4ddb99647faf6e11e6991a
GitHub Action <action@github.com>
2026-06-13 06:36:44 +0000

    Auto-sync Firestore data and update version [skip ci]

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260613.0634";
+const APP_VERSION = "20260613.0636";
 console.info(`COL_ENG Intake App Version: ${APP_VERSION} (Firebase 11.10.0)`);
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
